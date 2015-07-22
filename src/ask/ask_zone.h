34a9d0ab19f6ac136324c8784b9cda191dca0e9e
dooqu <51045352@qq.com>
2015-07-22 21:51:23 +0800

    ask_zone

---
+++ src/ask/ask_zone.h
@@ -0,0 +1,41 @@
+#ifndef __ASK_ZONE_H__
+#define __ASK_ZONE_H__
+
+#include "../service/game_zone.h"
+#include "questions.h"
+
+namespace dooqu_server
+{
+	namespace plugins
+	{
+		using namespace dooqu_server::service;
+
+		class ask_zone : public game_zone
+		{
+		protected:
+			question_collection questions_;
+
+			void on_load()
+			{
+				questions_.fill();
+
+				game_zone::on_load();
+			}
+
+			void on_unload()
+			{
+				questions_.clear();
+
+				game_zone::on_unload();
+			}
+
+		public:
+			ask_zone(game_service* service, char* zone_id) : game_zone(service, zone_id)
+			{
+
+			}
+		};
+	}
+}
+
+#endif
